cf8f08d1ed1b9454ac591022c6258b015eacfedb
Pranav Kaushik <pranav7kaushik@gmail.com>
2023-08-03 23:36:10 +0530

    Added viewset to profiles api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 class UserProfileManager(BaseUserManager):
 	"""Manager for user profiles"""
--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -2,5 +2,5 @@ from rest_framework import serializers
 
 
 class HelloSerializer(serializers.Serializer):
-	"""Serializes a name field for testing our APIView"""
+	"""Serializes a name field for testing the APIView"""
 	name = serializers.CharField(max_length=10)
